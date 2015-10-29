faf00192fe58bbb2ef40b6e881d2333ed38f2322
Michał Kujbida <michal@edarex.pl>
2015-10-29 10:42:45 +0100

    Dodanie brakujących plików

---
+++ ProjectManager/ProjectManager.Services/DictionaryRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectManager.Domain;
+using ProjectManager.Services.Interfaces;
+
+namespace ProjectManager.Services
+{
+    public class DictionaryRepository : IDictionaryRepository
+    {
+        private readonly IDictionaryRepository _dictionaryRepository;
+        public DictionaryRepository(IDictionaryRepository dictionaryRepository)
+        {
+            this._dictionaryRepository = dictionaryRepository;
+        }
+        public IList<Priority> GetPriorities()
+        {
+            return _dictionaryRepository.GetPriorities().ToList();
+        }
+
+        public IList<Status> GetStatuses()
+        {
+            return _dictionaryRepository.GetStatuses().ToList();
+        }
+
+        public IList<Category> GetCategories()
+        {
+            return _dictionaryRepository.GetCategories().ToList();
+        }
+    }
+}
